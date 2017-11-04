545e0f8917aefa6a8c58c2a7f3c03862168fee0a
mrliber8 <33376476+mrliber8@users.noreply.github.com>
2017-11-04 17:30:24 +0100

    Add files via upload

---
+++ start.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+<link href="style1.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="dropdown" style="float:right;">
+      <button class="dropbtn">Menu</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+
+    <div class="header">
+      <div class="container">
+        <p>Startpagina van de website. Gemoddeleerd naar mijn mockup. In het menu kunnen de andere paginas staan. Moet het Adres invullen veld nog maken.</p>
+      </div>
+    </div>
+
+
+    <div class="startpage">
+      <div class="container">
+        <h1>FeedMeNow</h1>
+        <p>Gemakkelijk eten bestellen vanaf uw stoel!</p>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="container">
+        <img src="https://i.imgur.com/zpyjRIU.png?1">
+        <h2>Website FeedMeNow</h2>
+        <p>Uitleggen wat de afbeeldingen betekenen, ook kleiner maken.</p>
+      </div>
+    </div>
+  </body>
+</html>
+
+<div class="footer">
+  <div class="container">
+    <p>&copy; Website FeedMeNow</p>
+  </div>
+</div>
+</body>
+</html>
